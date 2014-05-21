3a9958998250cc3ed7b46132b033c675d8a1b7fb
Jack Cutting <me@jackcutting.co.uk>
2014-05-21 10:18:04 +0100

    Added social to single.php

--- single.php
+++ single.php
@@ -6,6 +6,31 @@
 
 	<?php the_content(); ?>
 
+	<div class="divider"></div>
+
+	<!-- AddThis Button BEGIN -->
+	<div class="addthis_toolbox addthis_default_style ">
+	<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+	<a class="addthis_button_tweet"></a>
+	<a class="addthis_button_pinterest_pinit"></a>
+	<a class="addthis_counter addthis_pill_style"></a>
+	</div>
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-517a68100f09713f"></script>
+	<!-- AddThis Button END -->
+
+	<div class="divider"></div>
+
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<div class="fb-comments" data-href="<?php the_permalink(); ?>" data-width="665px" data-num-posts="10"></div>
+
 <?php endwhile; else: ?>
 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 <?php endif; wp_reset_query(); ?>
